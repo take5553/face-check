b42e6507f2a6f623755b6573d5b47ed3fb46aea8
Takeshi <yamazakitakeshi02@gmail.com>
2021-08-14 19:25:21 +0900

    create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+class CaptureWindow(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+if __name__="__main__":
+    window = tk.Tk()
+    app = CaptureWindow(master=window)
+    app.mainloop()
+    
