f8b16cdf43ecbe4b5ec34eaea543de39f9cafd03
Takeshi <yamazakitakeshi02@gmail.com>
2021-08-19 23:07:47 +0900

    add one shot capture

--- src/capture_window.py
+++ src/capture_window.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from jetcam.usb_camera import USBCamera
 import cv2
 import PIL.Image, PIL.ImageTk
+import os
 
 class CaptureWindow(ttk.Frame):
     def __init__(self, master=None, camera=None, cap_device=0, cap_width=352, cap_height=288, delay=10):
@@ -19,6 +20,21 @@ class CaptureWindow(ttk.Frame):
         self.delay = delay
 
         self.create_widgets()
+        
+        self.capIm = False
+        self.path = "data/one_shot/"
+        self.fileprefix = "capture"
+        self.fileext = ".jpg"
+        self.image_index = 0
+        files = os.listdir(self.path)
+        files_file = [f for f in files if os.path.isfile(os.path.join(self.path, f))]
+        if len(files_file) > 0:
+            for f in files_file:
+                if (int(f[len(self.fileprefix):len(self.fileprefix) + 4]) > self.image_index):
+                    self.image_index = int(f[len(self.fileprefix):len(self.fileprefix) + 4]) + 1
+        print('image_index: {}'.format(self.image_index))
+        
+        self.capIms = False
 
         if self.camera == None:
             self.camera = USBCamera(capture_device=self.cap_device, capture_width=self.cap_width, capture_height=self.cap_height, width=self.cap_width, height=self.cap_height)
@@ -33,11 +49,27 @@ class CaptureWindow(ttk.Frame):
         self.canvas1 = tk.Canvas(self, width = self.cap_width, height = self.cap_height)
         self.canvas1.grid(column=0, row=0, sticky=(tk.W, tk.E), padx=10, pady=10)
 
-        # #Button Frame
-        # self.frame1 = ttk.Frame(self)
-        # self.frame1.grid(column=0, row=1, sticky=(tk.W, tk.E), padx=10, pady=10)
+        #Button Frame
+        self.frame1 = ttk.Frame(self)
+        self.frame1.grid(column=0, row=1, sticky=(tk.W, tk.E))
 
-        # #Capture Button
+        #One Image Button
+        self.button1 = ttk.Button(self.frame1, text="Take a pic", command=self.OneCapture)
+        self.button1.grid(column=0, row=0, padx=100, pady=40, ipady=20, sticky=(tk.W, tk.E))
+
+        #Images Button
+        self.button2 = ttk.Button(self.frame1, text="Continous images", command=self.CaptureImages)
+        self.button2.grid(column=1, row=0, padx=100, pady=40, ipady=20, sticky=(tk.W, tk.E))
+
+        self.frame1.columnconfigure(0, weight=1)
+        self.frame1.columnconfigure(1, weight=1)
+        self.frame1.rowconfigure(0, weight=1)
+        
+    def OneCapture(self):
+        self.capIm = True
+
+    def CaptureImages(self):
+        self.capIms = True
 
     def closing_window(self):
         self.camera.running = False
@@ -45,6 +77,10 @@ class CaptureWindow(ttk.Frame):
 
     def update(self):
         frame = self.camera.value
+        if self.capIm == True:
+            cv2.imwrite('{}{}{:04}{}'.format(self.path, self.fileprefix, self.image_index, self.fileext), frame)
+            self.image_index += 1
+            self.capIm = False
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
         self.canvas1.create_image(0, 0, image = self.photo, anchor = tk.NW)
