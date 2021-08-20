cd4106cd6fe4568f56718b0e055969d576f7d82b
Takeshi <yamazakitakeshi02@gmail.com>
2021-08-20 23:41:37 +0900

    add continuous capture function

--- src/capture_window.py
+++ src/capture_window.py
@@ -19,72 +19,85 @@ class CaptureWindow(ttk.Frame):
         self.cap_height = cap_height
         self.delay = delay
 
-        self.create_widgets()
+        self._create_widgets()
         
         self.capIm = False
-        self.path = "data/one_shot/"
+        self.abspath = os.path.dirname(os.path.abspath(__file__)) + "/data/"
+        self.imdir = "one_shot/"
         self.fileprefix = "capture"
         self.fileext = ".jpg"
         self.image_index = 0
-        files = os.listdir(self.path)
-        files_file = [f for f in files if os.path.isfile(os.path.join(self.path, f))]
-        if len(files_file) > 0:
-            for f in files_file:
-                if (int(f[len(self.fileprefix):len(self.fileprefix) + 4]) > self.image_index):
-                    self.image_index = int(f[len(self.fileprefix):len(self.fileprefix) + 4]) + 1
+        self._get_index()
         
         self.capIms = False
 
         if self.camera == None:
             self.camera = USBCamera(capture_device=self.cap_device, capture_width=self.cap_width, capture_height=self.cap_height, width=self.cap_width, height=self.cap_height)
         self.camera.running = True
-        self.master.protocol("WM_DELETE_WINDOW", self.closing_window)
-        self.running = True
-        self.update()
+        self.master.protocol("WM_DELETE_WINDOW", self._on_closing)
+        self._update()
 
-    def create_widgets(self):
+    def _create_widgets(self):
 
         #Canvas
         self.canvas1 = tk.Canvas(self, width = self.cap_width, height = self.cap_height)
-        self.canvas1.grid(column=0, row=0, sticky=(tk.W, tk.E), pady=10)
+        self.canvas1.grid(column=0, row=0, sticky=(tk.W, tk.E))
 
         #Button Frame
         self.frame1 = ttk.Frame(self)
         self.frame1.grid(column=0, row=1, sticky=(tk.W, tk.E))
 
         #One Image Button
-        self.button1 = ttk.Button(self.frame1, text="Take a pic", command=self.OneCapture)
+        self.button1 = ttk.Button(self.frame1, text="Take a pic", command=self._one_capture)
         self.button1.grid(column=0, row=0, padx=60, pady=40, ipady=20, sticky=(tk.W, tk.E))
 
         #Images Button
-        self.button2 = ttk.Button(self.frame1, text="Continous images", command=self.CaptureImages)
+        self.button2 = ttk.Button(self.frame1, text="Continous images", command=self._capture_images)
         self.button2.grid(column=1, row=0, padx=60, pady=40, ipady=20, sticky=(tk.W, tk.E))
 
         self.frame1.columnconfigure(0, weight=1)
         self.frame1.columnconfigure(1, weight=1)
         self.frame1.rowconfigure(0, weight=1)
+
+    def _get_index(self):
+        files = os.listdir(self.abspath + self.imdir)
+        files_file = [f for f in files if os.path.isfile(os.path.join(self.abspath + self.imdir, f))]
+        if len(files_file) > 0:
+            for f in files_file:
+                if (int(f[len(self.fileprefix):len(self.fileprefix) + 4]) > self.image_index):
+                    self.image_index = int(f[len(self.fileprefix):len(self.fileprefix) + 4]) + 1
+        else:
+            self.image_index = 0
         
-    def OneCapture(self):
-        self.capIm = True
+    def _one_capture(self):
+        if self.capIms == False:
+            self.capIm = True
+            self.imdir = 'one_shot/'
+            self._get_index()
 
-    def CaptureImages(self):
-        self.capIms = True
+    def _capture_images(self):
+        if self.capIm == True:
+            self.capIm = False
+        self.capIms = not self.capIms
+        if self.capIms == True:
+            self.imdir = 'Takeshi/train/'
+            self._get_index()
 
-    def closing_window(self):
+    def _on_closing(self):
         self.camera.running = False
         self.master.destroy()
 
-    def update(self):
+    def _update(self):
         frame = self.camera.value
-        if self.capIm == True:
-            cv2.imwrite('{}{}{:04}{}'.format(self.path, self.fileprefix, self.image_index, self.fileext), frame)
+        if (self.capIm == True) or (self.capIms == True):
+            cv2.imwrite('{}{}{:04}{}'.format(self.abspath + self.imdir, self.fileprefix, self.image_index, self.fileext), frame)
             self.image_index += 1
-            self.capIm = False
+            if (self.capIm == True):
+                self.capIm = False
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
         self.canvas1.create_image(0, 0, image = self.photo, anchor = tk.NW)
-        self.master.after(self.delay, self.update)
-
+        self.master.after(self.delay, self._update)
 
 if __name__ == "__main__":
     window = tk.Tk()
