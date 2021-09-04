f1991e55660af5eef5147201f82919222b3a3203
Takeshi <yamazakitakeshi02@gmail.com>
2021-09-04 11:04:11 +0900

    rearrange config window controls again

--- src/w_config.py
+++ src/w_config.py
@@ -26,128 +26,105 @@ class ConfigWindow(ttk.Frame):
 
     def _create_widgets(self):
         
-        # Main Grid
+        padx = 15
+        pady = 10
+
         row = 0
-        
-        ## Capture Settings
+
         column = 0
         self._label_cap = ttk.Label(self, text="Capture")
-        self._label_cap.grid(column=column, row=row, padx=10, pady=10, sticky=tk.N)
+        self._label_cap.grid(column=column, row=row, padx=padx, pady=pady)
         column += 1
-        self._frame_cap = ttk.Frame(self)
-        self._frame_cap.grid(column=column, row=row, padx=10, pady=10, sticky=(tk.W, tk.E))
+        self._label_dev = ttk.Label(self, text="Device")
+        self._label_dev.grid(column=column, row=row, padx=padx, pady=pady)
+        column += 1
+        self._combo_dev = ttk.Combobox(self, state="readonly", width=30)
+        self._combo_dev.bind("<<ComboboxSelected>>", self._combo_dev_on_selected)
+        self._combo_dev.grid(column=column, row=row, padx=padx, pady=pady)
         row += 1
         
-        ## Canvas Settings
+        column = 0
+        # blank cell
+        column += 1
+        self._label_res = ttk.Label(self, text="Resolution")
+        self._label_res.grid(column=column, row=row, padx=padx, pady=pady)
+        column += 1
+        self._combo_res = ttk.Combobox(self, state="readonly", width=30)
+        self._combo_res.bind("<<ComboboxSelected>>", self._combo_res_on_selected)
+        self._combo_res.grid(column=column, row=row, padx=padx, pady=pady)
+        row += 1
+        
+        column = 0
+        # blank cell
+        column += 1
+        self._label_rot = ttk.Label(self, text="Rotation")
+        self._label_rot.grid(column=column, row=row, padx=padx, pady=pady)
+        column += 1
+        self._combo_rot = ttk.Combobox(self, state="readonly", width=30)
+        self._combo_rot.bind("<<ComboboxSelected>>", self._combo_rot_on_selected)
+        self._combo_rot.grid(column=column, row=row, padx=padx, pady=pady)
+        row += 1
+        
+        column = 0
+        # blank cell
+        column += 1
+        self._label_fli = ttk.Label(self, text="Flip")
+        self._label_fli.grid(column=column, row=row, padx=padx, pady=pady)
+        column += 1
+        self._combo_fli = ttk.Combobox(self, state="readonly", width=30)
+        self._combo_fli.bind("<<ComboboxSelected>>", self._combo_fli_on_selected)
+        self._combo_fli.grid(column=column, row=row, padx=padx, pady=pady)
+        row += 1
+        
         column = 0
         self._label_can = ttk.Label(self, text="Canvas")
-        self._label_can.grid(column=column, row=row, padx=10, pady=10, sticky=tk.N)
+        self._label_can.grid(column=column, row=row, padx=padx, pady=pady)
         column += 1
-        self._frame_can = ttk.Frame(self)
-        self._frame_can.grid(column=column, row=row, padx=10, pady=10, sticky=(tk.W, tk.E))
-        row += 1
-        
-        ## Save
-        column = 0
-        
+        self._label_can_wxh = ttk.Label(self, text="Width x Height:")
+        self._label_can_wxh.grid(column=column, row=row, padx=padx, pady=pady)
         column += 1
-        self._frame_sav = ttk.Frame(self)
-        self._frame_sav.grid(column=column, row=row, padx=10, pady=10, sticky=tk.E)
-        self._label_sav = ttk.Label(self._frame_sav)
-        self._label_sav.grid(column=0, row=0, padx=20, sticky=tk.E)
-        self._button_sav = ttk.Button(self._frame_sav, text="Save", command=self._save)
-        self._button_sav.grid(column=1, row=0)
-        self._frame_sav.columnconfigure(0, weight=1)
-        row += 1
-        
-        # Capture Sub Grid
-        row = 0
-        
-        ## Device
-        column = 0
-        self._label_dev = ttk.Label(self._frame_cap, text="Device")
-        self._label_dev.grid(column=column, row=row, sticky=tk.W)
-        column += 1
-        self._combo_dev = ttk.Combobox(self._frame_cap, state="readonly", width=30)
-        self._combo_dev.bind("<<ComboboxSelected>>", self._combo_dev_on_selected)
-        self._combo_dev.grid(column=column, row=row, padx=5, sticky=tk.W)
-        row += 1
-        
-        ## Blank Row
-        self._frame_cap.rowconfigure(row, minsize=20)
-        row += 1
-        
-        ## Resolution
-        column = 0
-        self._label_res = ttk.Label(self._frame_cap, text="Resolution")
-        self._label_res.grid(column=column, row=row, sticky=tk.W)
-        column += 1
-        self._combo_res = ttk.Combobox(self._frame_cap, state="readonly", width=30)
-        self._combo_res.bind("<<ComboboxSelected>>", self._combo_res_on_selected)
-        self._combo_res.grid(column=column, row=row, padx=5, sticky=tk.W)
-        row += 1
-        
-        ## Blank Row
-        self._frame_cap.rowconfigure(row, minsize=20)
-        row += 1
-        
-        ## Rotation
-        column = 0
-        self._label_rot = ttk.Label(self._frame_cap, text="Rotation")
-        self._label_rot.grid(column=column, row=row, sticky=tk.W)
-        column += 1
-        self._combo_rot = ttk.Combobox(self._frame_cap, state="readonly", width=30)
-        self._combo_rot.bind("<<ComboboxSelected>>", self._combo_rot_on_selected)
-        self._combo_rot.grid(column=column, row=row, padx=5, sticky=tk.W)
-        row += 1
-        
-        ## Blank Row
-        self._frame_cap.rowconfigure(row, minsize=20)
-        row += 1
-        
-        ## Flip
-        column = 0
-        self._label_fli = ttk.Label(self._frame_cap, text="Flip")
-        self._label_fli.grid(column=column, row=row, sticky=tk.W)
-        column += 1
-        self._combo_fli = ttk.Combobox(self._frame_cap, state="readonly", width=30)
-        self._combo_fli.bind("<<ComboboxSelected>>", self._combo_fli_on_selected)
-        self._combo_fli.grid(column=column, row=row, padx=5, sticky=tk.W)
-        row += 1
-        
-        # Canvas Sub Grid
-        row = 0
-        
-        ## Canvas Size
-        column = 0
-        self._label_can_wxh = ttk.Label(self._frame_can, text="Width x Height:")
-        self._label_can_wxh.grid(column=column, row=row, sticky=tk.W)
-        column += 1
-        self._frame_can_wxh = ttk.Frame(self._frame_can)
-        self._frame_can_wxh.grid(column=column, row=row, padx=5, sticky=tk.W)
+        self._frame_can_wxh = ttk.Frame(self)
+        self._frame_can_wxh.grid(column=column, row=row, padx=padx, pady=pady, sticky=tk.W)
+        # -----------
         self._entry_can_width = ttk.Entry(self._frame_can_wxh, width=6)
         self._entry_can_width.grid(column=0, row=0)
         self._label_can_mul = ttk.Label(self._frame_can_wxh, text=' x ')
         self._label_can_mul.grid(column=1, row=0)
         self._entry_can_height = ttk.Entry(self._frame_can_wxh, width=6)
         self._entry_can_height.grid(column=2, row=0)
+        # -----------
         row += 1
         
-        ## Blank Row
-        self._frame_can.rowconfigure(row, minsize=20)
-        row += 1
-        
-        ## FPS
         column = 0
-        self._label_can_fps = ttk.Label(self._frame_can, text="Update Interval:")
-        self._label_can_fps.grid(column=column, row=row, sticky=tk.W)
+        # blank cell
         column += 1
-        self._frame_can_fps = ttk.Frame(self._frame_can)
-        self._frame_can_fps.grid(column=column, row=row, padx=5, sticky=tk.W)
+        self._label_can_fps = ttk.Label(self, text="Update Interval:")
+        self._label_can_fps.grid(column=column, row=row, padx=padx, pady=pady)
+        column += 1
+        self._frame_can_fps = ttk.Frame(self)
+        self._frame_can_fps.grid(column=column, row=row, padx=padx, pady=pady, sticky=tk.W)
+        # ----------
         self._entry_can_fps = ttk.Entry(self._frame_can_fps, width=4)
         self._entry_can_fps.grid(column=0, row=0)
         self._label_can_fpsunit = ttk.Label(self._frame_can_fps, text="ms")
         self._label_can_fpsunit.grid(column=1, row=0)
+        # ----------
+        row += 1
+        
+        column = 0
+        # blank cell
+        column += 1
+        # blank cell
+        column += 1
+        self._frame_sav = ttk.Frame(self)
+        self._frame_sav.grid(column=column, row=row, padx=padx, pady=pady, sticky=tk.E)
+        # ----------
+        self._label_sav = ttk.Label(self._frame_sav)
+        self._label_sav.grid(column=0, row=0, padx=20, sticky=tk.E)
+        self._button_sav = ttk.Button(self._frame_sav, text="Save", command=self._save)
+        self._button_sav.grid(column=1, row=0)
+        self._frame_sav.columnconfigure(0, weight=1)
+        # ----------
         row += 1
         
         # Bind Variables
