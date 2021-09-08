a6a3ce316ff956a1701eb2047d52c2a5454b4317
Takeshi <yamazakitakeshi02@gmail.com>
2021-09-08 23:58:10 +0900

    add save state information and shutter timing on capture window

--- src/w_capture.py
+++ src/w_capture.py
@@ -61,21 +61,44 @@ class CaptureWindow(ttk.Frame):
         self._button_images = ttk.Button(self._frame1, text="Continous images", command=self._capture_images)
         self._button_images.grid(column=1, row=0, padx=10, pady=10, sticky=(tk.W, tk.E))
         
-        # Image Name
+        # Save Settings
         self._frame_name = ttk.Frame(self._frame1)
         self._frame_name.grid(column=0, row=1, padx=10, pady=10, sticky=(tk.W, tk.E))
         self._label_name = ttk.Label(self._frame_name, text='Data Name')
         self._label_name.grid(column=0, row=0)
         self._entry_name = ttk.Entry(self._frame_name)
         self._entry_name.grid(column=1, row=0, sticky=(tk.W, tk.E))
+        self._label_speed = ttk.Label(self._frame_name, text='Save speed')
+        self._label_speed.grid(column=0, row=1)
+        self._frame_speed = ttk.Frame(self._frame_name)
+        self._frame_speed.grid(column=1, row=1)
+        self._label_speed_despription = ttk.Label(self._frame_speed, text='1 shoot per ')
+        self._label_speed_despription.grid(column=0, row=0)
+        self._entry_speed = ttk.Entry(self._frame_speed)
+        self._entry_speed.grid(column=1, row=0, sticky=(tk.W, tk.E))
+        self._label_speed_despription2 = ttk.Label(self._frame_speed, text='frame(s)')
+        self._label_speed_despription2.grid(column=2, row=0)
         
         # Save State
         self._frame_save_state = ttk.Frame(self._frame1)
         self._frame_save_state.grid(column=1, row=1, padx=10, pady=10, sticky=(tk.W, tk.E))
-        self._label_save_state = ttk.Label(self._frame_save_state, text='Taken Pics:')
-        self._label_save_state.grid(column=0, row=0)
+        self._label_save_description = ttk.Label(self._frame_save_state, text='Taken Pics : ')
+        self._label_save_description.grid(column=0, row=0)
         self._label_save_count = ttk.Label(self._frame_save_state)
         self._label_save_count.grid(column=1, row=0)
+        self._label_sub_count = []
+        self._label_train_description = ttk.Label(self._frame_save_state, text='Train : ')
+        self._label_train_description.grid(column=0, row=1, pady=2)
+        self._label_sub_count += [ttk.Label(self._frame_save_state)]
+        self._label_sub_count[0].grid(column=1, row=1, pady=2)
+        self._label_valid_description = ttk.Label(self._frame_save_state, text='Valid : ')
+        self._label_valid_description.grid(column=0, row=2, pady=2)
+        self._label_sub_count += [ttk.Label(self._frame_save_state)]
+        self._label_sub_count[1].grid(column=1, row=2, pady=2)
+        self._label_test_description = ttk.Label(self._frame_save_state, text='Test : ')
+        self._label_test_description.grid(column=0, row=3, pady=2)
+        self._label_sub_count += [ttk.Label(self._frame_save_state)]
+        self._label_sub_count[2].grid(column=1, row=3, pady=2)
 
         self._frame1.columnconfigure(0, weight=1)
         self._frame1.columnconfigure(1, weight=1)
@@ -85,6 +108,8 @@ class CaptureWindow(ttk.Frame):
         
         self._data_name = tk.StringVar()
         self._entry_name.configure(textvariable=self._data_name)
+        self._shutter_speed = tk.IntVar(value=1)
+        self._entry_speed.configure(textvariable=self._shutter_speed)
         
 
     def _get_index(self):
@@ -110,6 +135,8 @@ class CaptureWindow(ttk.Frame):
     def _one_capture(self):
         if self._cap_ims_fl == False:
             self._cap_im_fl = True
+            self._timing = 0
+            self._shutter_timing = 0
             self._get_index()
 
 
@@ -118,7 +145,14 @@ class CaptureWindow(ttk.Frame):
             self._cap_im_fl = False
         self._cap_ims_fl = not self._cap_ims_fl
         if self._cap_ims_fl == True:
+            self._button_images.configure(text="Stop")
+            self._button_image.configure(state="disable")
             self._get_index()
+            self._timing = 0
+            self._shutter_timing = self._shutter_speed.get()
+        else:
+            self._button_images.configure(text="Continous images")
+            self._button_image.configure(state="enable")
 
 
     def _on_closing(self):
@@ -143,20 +177,24 @@ class CaptureWindow(ttk.Frame):
 
     def _update(self):
         frame = self._camera.value
-        if (self._cap_im_fl == True) or (self._cap_ims_fl == True):
+        if self._cap_ims_fl == True:
+            self._timing += 1
+        if (self._cap_im_fl == True) or ((self._cap_ims_fl == True) and (self._timing == self._shutter_timing)):
+            self._timing = 0
             self._total_im_count += 1
+            self._label_save_count.configure(text=self._total_im_count)
             index = self._total_im_count % 10
             if index == 0:
                 self._save_dir_order = []
                 self._set_save_dir_order()
             cv2.imwrite('{}{}{:04}{}'.format(self._abspath + self._im_dir + self._datasets_dir[self._save_dir_order[index]], self._file_prefix, self._image_index[self._save_dir_order[index]], self._file_ext), frame)
             self._image_index[self._save_dir_order[index]] += 1
+            self._label_sub_count[self._save_dir_order[index]].configure(text=self._image_index[self._save_dir_order[index]])
             if (self._cap_im_fl == True):
                 self._cap_im_fl = False
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self._photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
         self._canvas1.create_image(0, 0, image = self._photo, anchor = tk.NW)
-        self._label_save_count.configure(text=self._total_im_count)
         self.master.after(self._delay, self._update)
 
 
