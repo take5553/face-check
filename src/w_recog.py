d6999452e0286030e1abc0dbab8ca0be8e56d73e
Takeshi <yamazakitakeshi02@gmail.com>
2021-09-13 21:50:42 +0900

    add facenet.detect on recognition window
    If the size of captured image is too big, the app looks freeze (and sometimes it really freezes). To confirm it, pre-detection is set before camera starts working and detection starts after pushing start button so that you can see if the camera works correctly.

--- src/w_recog.py
+++ src/w_recog.py
@@ -1,9 +1,12 @@
 import cv2
-import PIL.Image, PIL.ImageTk
+import PIL.Image, PIL.ImageTk, PIL.ImageDraw
 import tkinter as tk
 from tkinter import ttk
 import json_util as ju
 from mycamera import MyCamera
+
+from facenet_pytorch import MTCNN
+import torch
 
 class RecogWindow(ttk.Frame):
     def __init__(self, master=None):
@@ -19,6 +22,9 @@ class RecogWindow(ttk.Frame):
         self._delay = self._settings['canvas_settings']['update_interval']
         
         self._create_widgets()
+        self._setup_nn_engine()
+        
+        self._detecting = False
         
         self._camera.running = True
         self.master.protocol("WM_DELETE_WINDOW", self._on_closing)
@@ -31,17 +37,44 @@ class RecogWindow(ttk.Frame):
         self._canvas1 = tk.Canvas(self, width = self._canvas_width, height = self._canvas_height)
         self._canvas1.grid(column=0, row=0, sticky=(tk.W, tk.E))
         
+        # Button
+        self._button_start = ttk.Button(self, text='Start', command=self._start_detection)
+        self._button_start.grid(column=0, row=1, sticky=(tk.W, tk.E))
+        
+    
+    def _setup_nn_engine(self):
+        print('Initializing start')
+        self._nn_device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print('Running on device: {}'.format(self._nn_device))
+        self._mtcnn = MTCNN(keep_all=True, device=self._nn_device)
+        print('MTCNN initialized')
+        dummy = self._camera.read()
+        print('Dummy data captured')
+        dummy = cv2.cvtColor(dummy, cv2.COLOR_BGR2RGB)
+        self._mtcnn.detect(dummy)
+        print('Pre-detection complete')
+        
         
     def _on_closing(self):
         self._camera.running = False
         self._camera.cap.release()
         self.master.destroy()
         
+        
+    def _start_detection(self):
+        self._detecting = not self._detecting
+        
     
     def _update(self):
         frame = self._camera.value
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self._photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+        image = PIL.Image.fromarray(frame)
+        if self._detecting == True:
+            boxes, _ = self._mtcnn.detect(image)
+            draw = PIL.ImageDraw.Draw(image)
+            for box in boxes:
+                draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
+        self._photo = PIL.ImageTk.PhotoImage(image=image)
         self._canvas1.create_image(0, 0, image = self._photo, anchor = tk.NW)
         self.master.after(self._delay, self._update)
 
