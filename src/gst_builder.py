e69188b2a92d16f784443a6bf2360e34b8760f43
Takeshi <yamazakitakeshi02@gmail.com>
2021-09-26 18:35:07 +0900

    add MySettings class and change setting.json's variable names

--- src/gst_builder.py
+++ src/gst_builder.py
@@ -1,45 +1,45 @@
 from device_check import get_device_id, get_format
 
 def get_gst(settings):
-    cap_device = get_device_id(settings['camera_mode'])[0]
+    cap_device = settings['camera_settings']['device_id']
     string = ''
 
-    if settings['camera_mode'] == 'usb':
+    if settings['camera_settings']['connection_method'] == 'usb':
         string += 'v4l2src device=/dev/video{}'.format(cap_device)
         # Capture
-        if settings['cap_settings']['cap_mode'] == 'MJPG':
+        if settings['capture_settings']['mode'] == 'MJPG':
             string += ' ! image/jpeg,width=(int){},height=(int){},framerate=(fraction){}/1 ! jpegdec' \
-                .format(settings['cap_settings']['cap_width'], settings['cap_settings']['cap_height'], int(settings['cap_settings']['cap_fps']))
+                .format(settings['capture_settings']['width'], settings['capture_settings']['height'], int(settings['capture_settings']['fps']))
         else:
             string += ' ! video/x-raw,width=(int){},height=(int){},framerate=(fraction){}/1' \
-                .format(settings['cap_settings']['cap_width'], settings['cap_settings']['cap_height'], int(settings['cap_settings']['cap_fps']))
+                .format(settings['capture_settings']['width'], settings['capture_settings']['height'], int(settings['capture_settings']['fps']))
         # Flip
-        if settings['cap_settings']['cap_rotation'] == 0:
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 1:
+        if settings['capture_settings']['rotation'] == 0:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 1:
             string += ' ! videoflip method=counterclockwise'
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 2:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 2:
             string += ' ! videoflip method=rotate-180'
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 3:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 3:
             string += ' ! videoflip method=clockwise'
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 4:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 4:
             string += ' ! videoflip method=horizontal-flip'
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 5:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 5:
             string += ' ! videoflip method=upper-right-diagonal'
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 6:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 6:
             string += ' ! videoflip method=vertical-flip'
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 7:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 7:
             string += ' ! videoflip method=upper-left-diagonal'
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
         # Crop
-        crop_height = int((int(cap_height) - int(settings['canvas_settings']['canvas_height'])) / 2)
-        crop_width = int((int(cap_width) - int(settings['canvas_settings']['canvas_width'])) / 2)
+        crop_height = int((int(cap_height) - int(settings['canvas_settings']['height'])) / 2)
+        crop_width = int((int(cap_width) - int(settings['canvas_settings']['width'])) / 2)
         if crop_height < 0:
             crop_height = 0
         if crop_width < 0:
@@ -48,30 +48,30 @@ def get_gst(settings):
             string += ' ! videocrop top={} bottom={} left={} right={}'.format(crop_height, crop_height, crop_width, crop_width)
         # Output
         string += ' ! videoconvert ! video/x-raw,format=(string)BGR ! appsink'
-    elif settings['camera_mode'] == 'csi':
+    elif settings['camera_settings']['connection_method'] == 'csi':
         # Capture
         string += 'nvarguscamerasrc sensor-id={}'.format(cap_device)
         string += ' ! video/x-raw(memory:NVMM), width={}, height={}, format=(string)NV12' \
-            .format(settings['cap_settings']['cap_width'], settings['cap_settings']['cap_height'])
+            .format(settings['capture_settings']['width'], settings['capture_settings']['height'])
         # Flip and Crop (with 90 degree rotatation or just flipping horizontally)
-        if settings['cap_settings']['cap_rotation'] == 0:
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 1:
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 2:
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 3:
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 4:
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 5:
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 6:
-            cap_height, cap_width = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        elif settings['cap_settings']['cap_rotation'] == 7:
-            cap_width, cap_height = settings['cap_settings']['cap_height'], settings['cap_settings']['cap_width']
-        crop_top = int((int(cap_height) - int(settings['canvas_settings']['canvas_height'])) / 2)
-        crop_left = int((int(cap_width) - int(settings['canvas_settings']['canvas_width'])) / 2)
+        if settings['capture_settings']['rotation'] == 0:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 1:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 2:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 3:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 4:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 5:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 6:
+            cap_height, cap_width = settings['capture_settings']['height'], settings['capture_settings']['width']
+        elif settings['capture_settings']['rotation'] == 7:
+            cap_width, cap_height = settings['capture_settings']['height'], settings['capture_settings']['width']
+        crop_top = int((int(cap_height) - int(settings['canvas_settings']['height'])) / 2)
+        crop_left = int((int(cap_width) - int(settings['canvas_settings']['width'])) / 2)
         if crop_top < 0:
             crop_top = 0
         if crop_left < 0:
@@ -79,12 +79,12 @@ def get_gst(settings):
         crop_bottom = int(cap_height) - crop_top
         crop_right = int(cap_width) - crop_left
         string += ' ! nvvidconv'
-        if settings['cap_settings']['cap_rotation'] != 0:
-            string += ' flip-method={}'.format(settings['cap_settings']['cap_rotation'])
+        if settings['capture_settings']['rotation'] != 0:
+            string += ' flip-method={}'.format(settings['capture_settings']['rotation'])
         if crop_top != 0 or crop_left != 0:
             string += ' left={} right={} top={} bottom={}'.format(crop_left, crop_right, crop_top, crop_bottom)
         # Output
         string += ' ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx' \
-            .format(settings['canvas_settings']['canvas_width'], settings['canvas_settings']['canvas_height'])
+            .format(settings['canvas_settings']['width'], settings['canvas_settings']['height'])
         string += ' ! videoconvert ! appsink'
     return string
---
+++ src/mysettings.py
@@ -0,0 +1,339 @@
+import json
+import os
+from device_check import get_device_id, get_format, get_device_list
+import gst_builder
+
+class MySettings():
+    
+    
+    _camera_settings = None
+    _capture_settings = None
+    _canvas_settings = None
+    _save_settings = None
+    _window_settings = None
+    _gst_str = ''
+    _setting_file_path = os.path.join(os.path.dirname(__file__), 'setting2.json')
+    _d = {}
+    
+    
+    def __init__(self):
+        self.load()
+        
+        
+    def load(self):
+        self._d = self._get_settings_dictionary()
+        self._camera_settings = _CameraSettings(self._d['camera_settings'])
+        self._capture_settings = _CaptureSettings(self._d['capture_settings'])
+        self._canvas_settings = _CanvasSettings(self._d['canvas_settings'])
+        self._save_settings = _SaveSettings(self._d['save_settings'])
+        self._window_settings = _WindowSettings(self._d['window_settings'])
+        self._gst_str = self._d['gst_str']
+        
+        
+    def save(self):
+        self._d = {
+            'camera_settings': {
+                'device_id' : self._camera_settings.device_id,
+                'connection_method' : self._camera_settings.connection_method
+            },
+            'capture_settings' : {
+                'mode' : self._capture_settings.mode,
+                'width' : self._capture_settings.width,
+                'height' : self._capture_settings.height,
+                'fps' : self._capture_settings.fps,
+                'rotation' : self._capture_settings.rotation
+            },
+            'canvas_settings' : {
+                'width' : self._canvas_settings.width,
+                'height' : self._canvas_settings.height,
+                'update_interval' : self._canvas_settings.update_interval
+            },
+            'save_settings' : {
+                'main_dir' : self._save_settings.main_dir,
+                'onepic_dir' : self._save_settings.onepic_dir
+            },
+            'window_settings' : {
+                'fullscreen' : self._window_settings.fullscreen
+            }
+        }
+        gst_str = gst_builder.get_gst(self._d)
+        self._d['gst_str'] = gst_str
+        with open(self._setting_file_path, 'w') as f:
+            json.dump(self._d, f, indent=4)
+        
+    @property
+    def camera(self):
+        return self._camera_settings
+    
+    
+    @property
+    def capture(self):
+        return self._capture_settings
+    
+    
+    @property
+    def canvas(self):
+        return self._canvas_settings
+    
+    
+    @property
+    def save_dir(self):
+        return self._save_settings
+    
+    
+    @property
+    def window(self):
+        return self._window_settings
+        
+        
+    @property
+    def gst_str(self):
+        return self._gst_str
+        
+        
+    def _get_settings_dictionary(self):
+        if os.path.exists(self._setting_file_path):
+            with open(self._setting_file_path, 'r') as f:
+                d = json.load(f)
+        else:
+            device_str = get_device_list()
+            if device_str == '':
+                raise RuntimeError('No device detected.')
+            device_id, connection_method = device_str.split()[0].split(':')
+            format_ = get_format(device_id)
+            resolution = format_.resolution()
+            d = {
+                'camera_settings': {
+                    'device_id' : device_id,
+                    'connection_method' : connection_method
+                },
+                'capture_settings' : {
+                    'mode' : format_.name,
+                    'width' : int(resolution[0]),
+                    'height' : int(resolution[1]),
+                    'fps' : float(resolution[2]),
+                    'rotation' : 0
+                },
+                'canvas_settings' : {
+                    'width' : 500,
+                    'height' : 400,
+                    'update_interval' : 30
+                },
+                'save_settings' : {
+                    'main_dir' : os.path.dirname(os.path.abspath(__file__)) + "/data/",
+                    'onepic_dir' : 'onepic/'
+                },
+                'window_settings' : {
+                    'fullscreen' : True
+                }
+            }
+            gst_str = gst_builder.get_gst(d)
+            d['gst_str'] = gst_str
+            with open(self._setting_file_path, 'w') as f:
+                json.dump(d, f, indent=4)
+        return d
+    
+    
+class _CameraSettings():
+    
+    
+    _device_id = ''
+    _connection_method = ''
+    
+    
+    def __init__(self, camera_settings):
+        self._device_id = camera_settings['device_id']
+        self._connection_method = camera_settings['connection_method']
+        
+        
+    @property
+    def device_id(self):
+        return self._device_id
+    
+    
+    @device_id.setter
+    def device_id(self, value):
+        self._device_id = value
+    
+    
+    @property
+    def connection_method(self):
+        return self._connection_method
+    
+    
+    @connection_method.setter
+    def connection_method(self, value):
+        self._connection_method = value
+        
+    
+class _CaptureSettings():
+    
+    
+    _mode = ''
+    _width = 0
+    _height = 0
+    _fps = 0.0
+    _rotation = 0
+    
+    
+    def __init__(self, capture_settings):
+        self._mode = capture_settings['mode']
+        self._width = capture_settings['width']
+        self._height = capture_settings['height']
+        self._fps = capture_settings['fps']
+        self._rotation = capture_settings['rotation']
+        
+    
+    @property
+    def mode(self):
+        return self._mode
+    
+    
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+    
+    
+    @property
+    def width(self):
+        return self._width
+    
+    
+    @width.setter
+    def width(self, value):
+        self._width = value
+    
+    
+    @property
+    def height(self):
+        return self._height
+    
+    
+    @height.setter
+    def height(self, value):
+        self._height = value
+    
+    
+    @property
+    def fps(self):
+        return self._fps
+    
+    
+    @fps.setter
+    def fps(self, value):
+        self._fps = value
+    
+    
+    @property
+    def rotation(self):
+        return self._rotation
+    
+    
+    @rotation.setter
+    def rotation(self, value):
+        self._rotation = value
+
+
+class _CanvasSettings():
+    
+    
+    _width = 0
+    _height = 0
+    _update_interval = 0
+    
+    
+    def __init__(self, canvas_settings):
+        self._width = canvas_settings['width']
+        self._height = canvas_settings['height']
+        self._update_interval = canvas_settings['update_interval']
+        
+        
+    @property
+    def width(self):
+        return self._width
+    
+    
+    @width.setter
+    def width(self, value):
+        self._width = value
+        
+        
+    @property
+    def height(self):
+        return self._height
+    
+    
+    @height.setter
+    def height(self, value):
+        self._height = value
+        
+        
+    @property
+    def update_interval(self):
+        return self._update_interval
+    
+    
+    @update_interval.setter
+    def update_interval(self, value):
+        self._update_interval = value
+        
+        
+class _SaveSettings():
+    
+    
+    _main_dir = ''
+    _onepic_dir = ''
+    
+    
+    def __init__(self, save_settings):
+        self._main_dir = save_settings['main_dir']
+        if self._main_dir[-1] != '/':
+            self._main_dir += '/'
+        self._onepic_dir = save_settings['onepic_dir']
+        if self._onepic_dir[-1] != '/':
+            self._onepic_dir += '/'
+        
+        
+    @property
+    def main_dir(self):
+        return self._main_dir
+    
+    
+    @main_dir.setter
+    def main_dir(self, value):
+        self._main_dir = value
+        
+        
+    @property
+    def onepic_dir(self):
+        return self._onepic_dir
+    
+    
+    @onepic_dir.setter
+    def onepic_dir(self, value):
+        self._onepic_dir = value
+        
+        
+    @property
+    def onepic_dir_fullpath(self):
+        return self._main_dir + self._onepic_dir
+        
+        
+class _WindowSettings():
+    
+    
+    _fullscreen = ''
+    
+    
+    def __init__(self, window_settings):
+        self._fullscreen = window_settings['fullscreen']
+        
+        
+    @property
+    def fullscreen(self):
+        return self._fullscreen
+    
+    
+    @fullscreen.setter
+    def fullscreen(self, value):
+        self._fullscreen = value
